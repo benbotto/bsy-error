2a1280bb8aa3d8bd0f0d0ee33d55d57e1d03add4
Ben Botto <benjamin.botto@gmail.com>
2020-08-06 09:52:27 -0700

    Exports PaymentRequiredError.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { DetailedError } from './error/DetailedError'
 export { DuplicateError } from './error/DuplicateError'
 export { ForbiddenError } from './error/ForbiddenError'
 export { NotFoundError } from './error/NotFoundError'
+export { PaymentRequiredError } from './error/PaymentRequiredError';
 export { UnauthorizedError } from './error/UnauthorizedError'
 export { ValidationErrorList } from './error/ValidationErrorList'
 export { ValidationError } from './error/ValidationError'
-
