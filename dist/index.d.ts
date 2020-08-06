c7ba32b2d4a904cda5848a51df48cd4791d8bb85
Ben Botto <benjamin.botto@gmail.com>
2020-08-06 07:20:35 -0700

    Remove/ignore dist folder.

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { DetailedError } from './error/DetailedError';
-export { DuplicateError } from './error/DuplicateError';
-export { ForbiddenError } from './error/ForbiddenError';
-export { NotFoundError } from './error/NotFoundError';
-export { UnauthorizedError } from './error/UnauthorizedError';
-export { ValidationErrorList } from './error/ValidationErrorList';
-export { ValidationError } from './error/ValidationError';
